c720bbe05f4847f4fd277615f3ddf3a15fe227c6
matheus1714 <matheussilvamartins1714@gmail.com>
2023-08-23 14:45:52 -0300

    configure style and theme

--- src/styles/theme/default.ts
+++ src/styles/theme/default.ts
@@ -1,17 +1,32 @@
 export const defaultTheme = {
-    colors:{
-        purpleNormal: '#8860E6',
-        purpleDark: '#5B409B',
-      
-        grayDark: '#202024',
-        grayLight: '#F3F4FE',
-        white: '#FFFFFF',
-      
-        sucess: '#04D361',
-        danger: '#FF8F8F',
-      
-        degreeLight: '#DEE0FC',
-        degreeNormal: '#996DFF',
-        degreeDark: '#BC9FFF',
-    }
-  }
+  colors: {
+    purpleLight: '#A881E6',
+    purple: '#7450AC',
+    purpleDark: '#523480',
+
+    gray100: '#FBF9FE',
+    gray200: '#AFABB6',
+    gray300: '#252529',
+    gray400: '#17171A',
+    gray500: '#111112',
+    gray600: '#0C0C0D',
+
+    successLight: '#4E995E',
+    success: '2F723D',
+
+    pink: '#DB5BBF',
+    pinkDark: '#251622',
+
+    orange: '#E07B67',
+    orangeDark: '#261A17',
+
+    yellow: '#BB9F3A',
+    yellowDark: '#211E12',
+
+    green: '#8CAD51',
+    greenDark: '#1C2015',
+
+    blue: '#7B94CB',
+    blueDark: '#1A1D23',
+  },
+}
